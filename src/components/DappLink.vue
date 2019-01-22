fd1fef0b185afa1d2b50a221a43ad97627dad523
Mona Lisa <Lenseg1@gmail.com>
2019-01-22 16:41:11 +0300

    dapp links

---
+++ src/components/DappLink.vue
@@ -0,0 +1,59 @@
+<template>
+  <button class="dapp-link" @click="emitClick" type="button" :title="title" name="button">
+    <img class="dapp-link__icon" :src="icon" :alt="name" />
+    <span class="dapp-link__name">{{name}}</span>
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'DappLink',
+
+  props: {
+    icon: {
+      name: String,
+      required: true,
+    },
+    name: {
+      name: String,
+      required: true,
+    },
+    title: {
+      name: String,
+      required: true,
+    },
+  },
+  methods: {
+    emitClick() {
+      this.$emit('click');
+    }
+  }
+
+};
+</script>
+
+<style lang="postcss">
+  .dapp-link {
+    background: transparent;
+    display: flex;
+    border: none;
+    cursor: pointer;
+    width: 120px;
+    &__icon {
+      flex: 0 0 auto;
+      display: flex;
+      max-width: 20px;
+    }
+    &__name {
+      display: block;
+      flex: 1 1 auto;
+      text-align:left;
+      line-height: 20px;
+      margin-left: 7px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+  }
+</style>
---
+++ src/constants/dapp-links.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    url: 'https://explorer.bounties.network/',
+    name: 'Bounties Explorer',
+    icon: 'https://explorer.bounties.network/favicon.ico',
+    title: 'Bounties Explorer: Find Open Bounties & Freelance Jobs Online'
+  },
+  {
+    url: 'https://www.cryptokitties.co/',
+    name: 'Cryptokitties',
+    icon: 'https://www.cryptokitties.co/favicon.ico',
+    title: 'Collect and breed digital cats!'
+  },
+  {
+    url: 'https://dogesontrial.dog/',
+    name: 'Dogesontrial',
+    icon: 'https://dogesontrial.dog/favicon.ico',
+    title: 'Doges on Trial'
+  }
+
+]
