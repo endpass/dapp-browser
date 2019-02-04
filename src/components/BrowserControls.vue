8d56a6c1f6a697b94e7f3250b1fadbb1f7a4e346
lamartire <lamartire@gmail.com>
2019-02-04 12:08:10 +0300

    feat: add user account settings

--- src/components/BrowserControls.vue
+++ src/components/BrowserControls.vue
@@ -2,9 +2,14 @@
   <div class="browser-controls">
     <form class="browser-controls__form" @submit.prevent="emitSubmit">
       <div class="browser-controls__input-wrapper">
-        <div v-if="address" class="browser-controls__avatar">
+        <button
+          v-if="authorized"
+          class="browser-controls__avatar"
+          type="button"
+          @click="emitAccount"
+        >
           <Identicon :address="address" />
-        </div>
+        </button>
         <div class="browser-controls__input">
           <VInput
             :value="value"
@@ -23,31 +28,30 @@
         >
           {{ buttonLabel }}
         </VButton>
-        <VButton v-if="authorized" @click="emitLogout"> Log out </VButton>
       </div>
       <Message v-if="error" :danger="true"> {{ error }} </Message>
     </form>
-    <div class="browser-controls__links">
+    <div v-if="authorized" class="browser-controls__links">
       <DappLink
         v-for="dapp in dapps"
         :key="dapp.url"
-        @click="goTo(dapp.url)"
         :url="dapp.url"
         :name="dapp.name"
         :title="dapp.title"
         :icon="dapp.icon"
+        @click="goTo(dapp.url)"
       ></DappLink>
     </div>
   </div>
 </template>
 
 <script>
+import dapps from '@/constants/dapp-links';
 import VButton from './VButton.vue';
 import VInput from './VInput.vue';
 import DappLink from './DappLink.vue';
 import Message from './Message.vue';
 import Identicon from './Identicon.vue';
-import dapps from '@/constants/dapp-links.js';
 
 export default {
   name: 'BrowserControls',
@@ -111,8 +115,10 @@ export default {
     },
 
     goTo(url) {
-      this.$emit('input', url);
-      this.$emit('submit');
+      if (!this.loading) {
+        this.$emit('input', url);
+        this.$emit('submit');
+      }
     },
 
     emitLogout() {
@@ -127,6 +133,10 @@ export default {
       if (!this.authorized) {
         this.$emit('auth');
       }
+    },
+
+    emitAccount() {
+      this.$emit('account');
     },
   },
 
@@ -155,7 +165,19 @@ export default {
 
 .browser-controls__avatar {
   flex: 0 0 auto;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  background: none;
+  border: none;
   margin-right: 16px;
+  cursor: pointer;
+  transition: opacity 0.25s;
+  opacity: 1;
+
+  &:hover {
+    opacity: 0.7;
+  }
 }
 
 .browser-controls__input {
