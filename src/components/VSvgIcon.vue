691020ff0d72a80936c34a1a91da2eb2915a8cd7
dudiq <idudiq@gmail.com>
2019-02-12 18:55:04 +0300

    refactor: move webpack conf to vue.conf

---
+++ src/components/VSvgIcon.vue
@@ -0,0 +1,55 @@
+<template>
+  <svg
+    :class="className"
+    :width="width"
+    :height="height"
+    :fill="fill"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title v-if="title">
+      {{ title }}
+    </title>
+    <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink" />
+  </svg>
+</template>
+
+<script>
+import get from 'lodash/get';
+
+export default {
+  name: 'VSvgIcon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: [String, Number],
+      default: '20px',
+    },
+    fill: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '20px',
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    iconPath() {
+      const block = require(`@/assets/icons/${this.name}.svg`);
+      console.log('block', block);
+      const url = get(block, 'default.url');
+      return url;
+    },
+    className() {
+      return `svg-icon svg-icon--${this.name}`;
+    },
+  },
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,53 @@
-module.exports = {
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { getEnv } = require('./env');
+
+/**
+ * Paths
+ */
+const { NODE_ENV = 'development', SOURCE_MAP, WEBPACK_MODE } = process.env;
+const ENV = getEnv(NODE_ENV);
+const PORT = process.env.PORT || 8080;
+const PUBLIC_PATH = path.resolve(__dirname, './public');
+const DIST_PATH = path.resolve(__dirname, './dist');
+
+const config = {
+  outputDir: DIST_PATH,
+  productionSourceMap: false,
   lintOnSave: false,
+
+  configureWebpack: {
+    devtool: SOURCE_MAP && 'cheap-module-eval-source-map',
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new SimpleProgressWebpackPlugin({
+        format: WEBPACK_MODE === 'development' ? 'minimal' : 'compact',
+      }),
+      new ExtractTextPlugin(
+        WEBPACK_MODE === 'development' ? 'main.css' : 'main.[hash:16].css',
+      ),
+      new HtmlWebpackPlugin({
+        template: path.resolve(PUBLIC_PATH, './index.html'),
+      }),
+      new webpack.DefinePlugin({
+        ENV: JSON.stringify(ENV),
+      }),
+    ],
+  },
+
+  chainWebpack: config => {
+    config.resolve.alias.set('@', path.resolve(__dirname, './src'));
+  },
   devServer: {
+    port: PORT,
+    contentBase: DIST_PATH,
+    hot: true,
+    historyApiFallback: true,
     proxy: {
       '/http': {
         target: 'http://localhost:8080',
@@ -10,3 +57,14 @@ module.exports = {
     },
   },
 };
+
+if (WEBPACK_MODE === 'development') {
+  config.configureWebpack.plugins.push(
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
+  );
+}
+
+
+module.exports = config;
